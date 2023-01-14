bbb389e54d41b536f0be8e62e040f8a87536b5e6
LiuY <1596087252@qq.com>
2023-01-14 17:46:47 +0000

    add l2d & calendar char components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import chart from './components/chart.vue';
+import l2d from './components/l2d.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <chart></chart>
+  <l2d></l2d>
 </template>
 
 <style scoped>
---
+++ src/components/l2d.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <canvas class="live2d" id="live2d" width="300" height="600"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  loadlive2d("live2d", "../../src/plugin/live2d/model/xiaomai/xiaomai.model.json");
+})
+
+</script>
+
+
+
+<style scoped>
+.live2d {
+  width: 100px;
+  height: auto;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  cursor: pointer;
+}
+</style>
